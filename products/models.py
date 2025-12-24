91faa7161d07ced94b0b282db62f43ef2844d3b8
Your Name <mix_2men@mail.ru>
2025-12-24 20:36:24 +0500

    добавил модели

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField()
+    brand = models.CharField(max_length=100)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+
